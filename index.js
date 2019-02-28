b7fdf4b5e41990cdd5f66e5a1468f9eaf3b07e74
Bao Pham <ryanvuepham@gmail.com>
2019-02-27 21:49:54 -0600

    init

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express')
+
+const db = require('./data/db')
+const server = express();
+
+const PORT = 9000
+
+server.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+server.listen(PORT, () => {
+console.log(`Server is listening on port ${PORT}`)
+})
